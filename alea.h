77fb8673938cafa71cd921371bc8dbc106bab1b2
etud <etud@etudiant3-pro-bookG2-640>
2018-10-16 11:57:09 +0200

    First step alea

---
+++ alea.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+class Alea 
+{
+    static int tirage;
+    static int nombre;
+
+    private:
+        int tableau[100];
+
+    public:
+        static long value(); 
+};
